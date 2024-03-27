741a1a8adac29da4292c62797f6bb9ba7bee5cb0
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2024-03-27 23:00:52 +0300

    Add files via upload

---
+++ xakaton/xakaton/Assets/SteamVR/InteractionSystem/Samples/JoeJeff/move.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool movingForward = false;
+
+    private void Update()
+    {
+        // Проверяем нажатие клавиш W или S для движения вперед или назад
+        float moveDirection = Input.GetAxis("Vertical");
+
+        // Движение вперед
+        if (moveDirection > 0)
+        {
+            movingForward = true;
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        // Движение назад
+        else if (moveDirection < 0)
+        {
+            movingForward = false;
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        }
+
+        // Переворачиваем фигурку при движении назад
+        if (!movingForward)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
